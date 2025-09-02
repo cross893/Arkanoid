fda282f0ac4c9bc2bc3e0605987142393ccd514e
cross893 <893cross893@gmail.com>
2025-09-02 20:52:36 +0500

    added class C_level

--- Engine.cpp
+++ Engine.cpp
@@ -1,6 +1,6 @@
 #include "Engine.h"
 
-char level_01[C_engine::level_height][C_engine::level_width] =
+char level_01[C_level::level_height][C_level::level_width] =
 {// Создание первого уровня
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -18,6 +18,7 @@ char level_01[C_engine::level_height][C_engine::level_width] =
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };// char level_01
 
+// C_ball
 //------------------------------------------------------------------------------------------------------------
 C_ball::C_ball()
 : ball_x_pos(20), ball_y_pos(170), ball_speed(3.0), ball_direction(M_PI - M_PI_4)
@@ -48,7 +49,7 @@ void C_ball::F_draw(HDC hdc, RECT& paint_area, C_engine *engine)
 
 
 //------------------------------------------------------------------------------------------------------------
-void C_ball::F_move(C_engine *engine)
+void C_ball::F_move(C_engine *engine, C_level *level)
 {
     int next_x_pos, next_y_pos;
     int max_x_pos_ball = C_engine::max_x_pos - ball_size;
@@ -62,7 +63,7 @@ void C_ball::F_move(C_engine *engine)
     // Отражение шарика от левой рамки
     if (next_x_pos < C_engine::border_x_offset)
     {
-        next_x_pos = C_engine::border_x_offset - (next_x_pos - C_engine::border_x_offset);
+        next_x_pos = C_engine::border_x_offset - (next_x_pos - C_engine::border_x_offset); //?
         ball_direction = M_PI - ball_direction;
     }// endif
 
@@ -97,7 +98,7 @@ void C_ball::F_move(C_engine *engine)
         }// endif
     }// endif
 
-    engine->F_check_level_brick_hit(next_y_pos);
+    level->F_check_level_brick_hit(next_y_pos, ball_direction);
 
     ball_x_pos = next_x_pos;
     ball_y_pos = next_y_pos;
@@ -114,6 +115,195 @@ void C_ball::F_move(C_engine *engine)
 
 
 
+// C_level
+//------------------------------------------------------------------------------------------------------------
+void C_level::F_check_level_brick_hit(int& next_y_pos, double &ball_direction)
+{
+    // Отражение шарика от кирпичей
+    int i, j;
+    int brick_y_pos = level_y_offset + level_height * cell_height;
+
+    for (i = level_height - 1; i >= 0; i--)
+    {
+        for (j = 0; j < level_width; j++)
+        {
+            if (level_01[i][j] == 0)
+                continue;
+
+            if (next_y_pos < brick_y_pos)
+            {
+                next_y_pos = brick_y_pos - (next_y_pos - brick_y_pos);
+                ball_direction = -ball_direction;
+            }// endif
+        }// end for
+        brick_y_pos -= cell_height;
+    }// end for
+}// int F_check_level_brick_hit
+
+
+
+
+//------------------------------------------------------------------------------------------------------------
+void C_level::F_draw_brick(HDC hdc, int x, int y, E_brick_type brick_type)
+{// Отрисовка кирпича
+    HPEN pen;
+    HBRUSH brush;
+
+    switch (brick_type)
+    {
+    case EBT_none:
+        return;
+
+
+    case EBT_light_red:
+        pen = pen_light_red;
+        brush = brush_light_red;
+        break;
+
+
+    case EBT_cyan:
+        pen = pen_cyan;
+        brush = brush_cyan;
+        break;
+
+
+    default:
+        return;
+    }// end switch
+
+    SelectObject(hdc, pen);
+    SelectObject(hdc, brush);
+
+    RoundRect(hdc, x * C_engine::global_scale, y * C_engine::global_scale, (x + brick_width) * C_engine::global_scale, (y + brick_height) * C_engine::global_scale, 2 * C_engine::global_scale, 2 * C_engine::global_scale);
+}// void F_draw_brick
+
+
+
+
+//------------------------------------------------------------------------------------------------------------
+void C_level::F_set_brick_letter_color(bool is_switch_color, HPEN &front_pen, HBRUSH &front_brush, HPEN &back_pen, HBRUSH &back_brush)
+{
+    if (is_switch_color)
+    {
+        front_pen = pen_light_red;
+        front_brush = brush_light_red;
+
+        back_pen = pen_cyan;
+        back_brush = brush_cyan;
+    }
+    else
+    {
+        front_pen = pen_cyan;
+        front_brush = brush_cyan;
+
+        back_pen = pen_light_red;
+        back_brush = brush_light_red;
+    }// endif
+}// void F_set_brick_letter_color
+
+
+
+
+//------------------------------------------------------------------------------------------------------------
+void C_level::F_draw_brick_letter(HDC hdc, int x, int y, E_brick_type brick_type, E_letter_type letter_type, int rotation_step)
+{// Отрисовка падающей буквы
+    bool    switch_color;
+    double  offset;
+    double  rotation_angle;
+    int     brick_half_height = brick_height * C_engine::global_scale / 2;
+    int     back_part_offset;
+
+    HPEN    front_pen, back_pen;
+    HBRUSH  front_brush, back_brush;
+    XFORM   xform, old_xform;
+
+    if (!(brick_type == EBT_cyan || brick_type == EBT_light_red))
+        return;
+
+    rotation_step = rotation_step % 16;
+
+    if (rotation_step < 8)
+        rotation_angle = 2.0 * M_PI / 16.0 * (double)rotation_step;
+    else
+        rotation_angle = 2.0 * M_PI / 16.0 * (double)(8 - rotation_step);
+
+    if (rotation_step > 4 && rotation_step <= 12)
+        switch_color = brick_type == EBT_cyan;
+    else
+        switch_color = brick_type == EBT_light_red;
+
+    F_set_brick_letter_color(switch_color, front_pen, front_brush, back_pen, back_brush);
+
+    if (rotation_step == 4 || rotation_step == 12)
+    {
+        SelectObject(hdc, back_pen);
+        SelectObject(hdc, back_brush);
+        Rectangle(hdc, x, y + brick_half_height - C_engine::global_scale, x + brick_width * C_engine::global_scale, y + brick_half_height);
+
+        SelectObject(hdc, front_pen);
+        SelectObject(hdc, front_brush);
+        Rectangle(hdc, x, y + brick_half_height, x + brick_width * C_engine::global_scale, y + brick_half_height + C_engine::global_scale - 1);
+    }
+    else
+    {
+        SetGraphicsMode(hdc, GM_ADVANCED);
+
+        xform.eM11 = 1.0f;
+        xform.eM12 = 0.0f;
+        xform.eM21 = 0.0f;
+        xform.eM22 = (float)cos(rotation_angle);
+        xform.eDx = (float)x;
+        xform.eDy = (float)y + (float)brick_half_height;
+        GetWorldTransform(hdc, &old_xform);
+        SetWorldTransform(hdc, &xform);
+
+        offset = 3.0 * (1.0 - fabs(xform.eM22)) * (double)C_engine::global_scale;
+        back_part_offset = (int)round(offset);
+
+        SelectObject(hdc, back_pen);
+        SelectObject(hdc, back_brush);
+        Rectangle(hdc, 0, -brick_half_height - back_part_offset, brick_width * C_engine::global_scale, brick_half_height - back_part_offset);
+
+        SelectObject(hdc, front_pen);
+        SelectObject(hdc, front_brush);
+        Rectangle(hdc, 0, -brick_half_height, brick_width * C_engine::global_scale, brick_half_height);
+
+        if (rotation_step > 4 && rotation_step <= 12)
+        {
+            if (letter_type == ELT_o)
+            {
+                SelectObject(hdc, pen_white_fat);
+                Ellipse(hdc, 0 + 5 * C_engine::global_scale, (-5 * C_engine::global_scale) / 2, 0 + 10 * C_engine::global_scale, (5 * C_engine::global_scale) / 2);
+            }// endif
+        }// endif
+
+        SetWorldTransform(hdc, &old_xform);
+    }// endif
+}// void F_draw_brick_letter
+
+
+
+
+//------------------------------------------------------------------------------------------------------------
+void C_level::F_draw(HDC hdc, RECT& paint_area)
+{// Отрисовка уровня
+    int i;
+    int j;
+
+    RECT intersection_rect;
+    
+    if (! IntersectRect(&intersection_rect, &paint_area, &level_rect))
+        return;
+
+    for (i = 0; i < level_height; i++)
+        for (j = 0; j < level_width; j++)
+            F_draw_brick(hdc, level_x_offset + j * cell_width, level_y_offset + i * cell_height, (E_brick_type)level_01[i][j]);
+}// void F_draw_level
+
+
+
+
+// C_engine
 //------------------------------------------------------------------------------------------------------------
 C_engine::C_engine()
 : inner_width(21), x_pos_platform(border_x_offset), x_step_platform(global_scale * 2), platform_width(28)
@@ -127,19 +317,21 @@ C_engine::C_engine()
 void C_engine::F_init_engine(HWND init_hwnd)
 {// Настройка игры при старте
     hwnd = init_hwnd;
-    pen_white_fat = CreatePen(PS_SOLID, global_scale, RGB(255, 255, 255));
+    pen_white_fat = CreatePen(PS_SOLID, global_scale, RGB(255, 255, 255)); //?
     F_create_pen_brush(15, 63, 31, pen_bg, brush_bg);
     F_create_pen_brush(255, 255, 255, pen_white, brush_white);
     F_create_pen_brush(0, 0, 0, pen_black, brush_black);
-    F_create_pen_brush(255, 85, 85, pen_light_red, brush_light_red);
-    F_create_pen_brush(85, 255, 255, pen_cyan, brush_cyan);
+    F_create_pen_brush(255, 85, 85, pen_light_red, brush_light_red); //?
+    F_create_pen_brush(85, 255, 255, pen_cyan, brush_cyan); //?
+    F_create_pen_brush(255, 85, 85, level.pen_light_red, level.brush_light_red); //?
+    F_create_pen_brush(85, 255, 255, level.pen_cyan, level.brush_cyan); //?
     F_create_pen_brush(151, 0, 0, pen_dark_red, brush_dark_red);
     F_create_pen_brush(0, 128, 192, pen_blue, brush_blue);
 
-    level_rect.left = level_x_offset * global_scale;
-    level_rect.top = level_y_offset * global_scale;
-    level_rect.right = level_rect.left + cell_width * level_width * global_scale;
-    level_rect.bottom = level_rect.top + cell_height * level_height * global_scale;
+    level.level_rect.left = C_level::level_x_offset * global_scale;
+    level.level_rect.top = C_level::level_y_offset * global_scale;
+    level.level_rect.right = level.level_rect.left + C_level::cell_width * C_level::level_width * global_scale;
+    level.level_rect.bottom = level.level_rect.top + C_level::cell_height * C_level::level_height * global_scale;
 
     F_redraw_platform();
 
@@ -150,12 +342,11 @@ void C_engine::F_init_engine(HWND init_hwnd)
 
 
 //------------------------------------------------------------------------------------------------------------
-void C_engine::F_draw_frame(HDC hdc, RECT& paint_area)
+void C_engine::F_draw_frame(HDC hdc, RECT &paint_area)
 {// Отрисовка игрового поля
     RECT intersection_rect;
 
-    if (IntersectRect(&intersection_rect, &paint_area, &level_rect))
-        F_draw_level(hdc);
+    level.F_draw(hdc, paint_area);
 
     if (IntersectRect(&intersection_rect, &paint_area, &platform_rect))
         F_draw_platform(hdc, x_pos_platform, y_pos_platform);
@@ -201,37 +392,10 @@ int C_engine::F_on_key_down(E_key_type key_type)
 //------------------------------------------------------------------------------------------------------------
 int C_engine::F_on_timer()
 {
-    ball.F_move(this);
+    ball.F_move(this, &level);
 
     return 0;
 }// int F_on_timer
-
-
-
-
-//------------------------------------------------------------------------------------------------------------
-void C_engine::F_check_level_brick_hit(int& next_y_pos)
-{
-    // Отражение шарика от кирпичей
-    int i, j;
-    int brick_y_pos = level_y_offset + level_height * cell_height;
-
-    for (i = level_height - 1; i >= 0; i--)
-    {
-        for (j = 0; j < level_width; j++)
-        {
-            if (level_01[i][j] == 0)
-                continue;
-
-            if (next_y_pos < brick_y_pos)
-            {
-                next_y_pos = brick_y_pos - (next_y_pos - brick_y_pos);
-                ball.ball_direction = -ball.ball_direction;
-            }// endif
-        }// end for
-        brick_y_pos -= cell_height;
-    }// end for
-}// int F_check_level_brick_hit
 
 
 
@@ -259,161 +423,6 @@ void C_engine::F_redraw_platform()
     InvalidateRect(hwnd, &prev_platform_rect, FALSE);
     InvalidateRect(hwnd, &platform_rect, FALSE);
 }// void F_redraw_platform
-
-
-
-
-//------------------------------------------------------------------------------------------------------------
-void C_engine::F_draw_brick(HDC hdc, int x, int y, E_brick_type brick_type)
-{// Отрисовка кирпича
-    HPEN pen;
-    HBRUSH brush;
-
-    switch (brick_type)
-    {
-    case EBT_none:
-        return;
-
-
-    case EBT_light_red:
-        pen =   pen_light_red;
-        brush = brush_light_red;
-        break;
-
-
-    case EBT_cyan:
-        pen =   pen_cyan;
-        brush = brush_cyan;
-        break;
-
-
-    default:
-        return;
-    }// end switch
-
-    SelectObject(hdc, pen);
-    SelectObject(hdc, brush);
-
-    RoundRect(hdc, x * global_scale, y * global_scale, (x + brick_width) * global_scale, (y + brick_height) * global_scale, 2 * global_scale, 2 * global_scale);
-}// void F_draw_brick
-
-
-
-
-//------------------------------------------------------------------------------------------------------------
-void C_engine::F_set_brick_letter_color(bool is_switch_color, HPEN &front_pen, HBRUSH &front_brush, HPEN &back_pen, HBRUSH &back_brush)
-{
-    if (is_switch_color)
-    {
-        front_pen =     pen_light_red;
-        front_brush =   brush_light_red;
-
-        back_pen =      pen_cyan;
-        back_brush =    brush_cyan;
-    }
-    else
-    {
-        front_pen =     pen_cyan;
-        front_brush =   brush_cyan;
-
-        back_pen =      pen_light_red;
-        back_brush =    brush_light_red;
-    }// endif
-}// void F_set_brick_letter_color
-
-
-
-
-//------------------------------------------------------------------------------------------------------------
-void C_engine::F_draw_brick_letter(HDC hdc, int x, int y, E_brick_type brick_type, E_letter_type letter_type, int rotation_step)
-{// Отрисовка падающей буквы
-    bool    switch_color;
-    double  offset;
-    double  rotation_angle;
-    int     brick_half_height = brick_height * global_scale / 2;
-    int     back_part_offset;
-
-    HPEN    front_pen, back_pen;
-    HBRUSH  front_brush, back_brush;
-    XFORM   xform, old_xform;
-
-    if (!(brick_type == EBT_cyan || brick_type == EBT_light_red))
-        return;
-
-    rotation_step = rotation_step % 16;
-
-    if (rotation_step < 8)
-        rotation_angle = 2.0 * M_PI / 16.0 * (double)rotation_step;
-    else
-        rotation_angle = 2.0 * M_PI / 16.0 * (double)(8 - rotation_step);
-
-    if (rotation_step > 4 && rotation_step <= 12)
-        switch_color = brick_type == EBT_cyan;
-    else
-        switch_color = brick_type == EBT_light_red;
-
-    F_set_brick_letter_color(switch_color, front_pen, front_brush, back_pen, back_brush);
-
-    if (rotation_step == 4 || rotation_step == 12)
-    {
-        SelectObject(hdc, back_pen);
-        SelectObject(hdc, back_brush);
-        Rectangle(hdc, x, y + brick_half_height - global_scale,  x + brick_width * global_scale, y + brick_half_height);
-
-        SelectObject(hdc, front_pen);
-        SelectObject(hdc, front_brush);
-        Rectangle(hdc, x, y + brick_half_height, x + brick_width * global_scale, y + brick_half_height + global_scale - 1);
-    }
-    else
-    {
-        SetGraphicsMode(hdc, GM_ADVANCED);
-
-        xform.eM11 =    1.0f;
-        xform.eM12 =    0.0f;
-        xform.eM21 =    0.0f;
-        xform.eM22 =    (float)cos(rotation_angle);
-        xform.eDx =     (float)x;
-        xform.eDy =     (float)y + (float)brick_half_height;
-        GetWorldTransform(hdc, &old_xform);
-        SetWorldTransform(hdc, &xform);
-
-        offset = 3.0 * (1.0 - fabs(xform.eM22)) * (double)global_scale;
-        back_part_offset = (int)round(offset);
-
-        SelectObject(hdc, back_pen);
-        SelectObject(hdc, back_brush);
-        Rectangle(hdc, 0, -brick_half_height - back_part_offset, brick_width * global_scale, brick_half_height - back_part_offset);
-
-        SelectObject(hdc, front_pen);
-        SelectObject(hdc, front_brush);
-        Rectangle(hdc, 0, -brick_half_height, brick_width * global_scale, brick_half_height);
-
-        if (rotation_step > 4 && rotation_step <= 12)
-        {
-            if (letter_type == ELT_o)
-            {
-                SelectObject(hdc, pen_white_fat);
-                Ellipse(hdc, 0 + 5 * global_scale, (-5 * global_scale) / 2, 0 + 10 * global_scale, (5 * global_scale) / 2);
-            }// endif
-        }// endif
-
-        SetWorldTransform(hdc, &old_xform);
-    }// endif
-}// void F_draw_brick_letter
-
-
-
-
-//------------------------------------------------------------------------------------------------------------
-void C_engine::F_draw_level(HDC hdc)
-{// Отрисовка уровня
-    int i;
-    int j;
-
-    for (i = 0; i < level_height; i++)
-        for (j = 0; j < level_width; j++)
-            F_draw_brick(hdc, level_x_offset + j * cell_width, level_y_offset + i * cell_height, (E_brick_type)level_01[i][j]);
-}// void F_draw_level
 
 
 
--- Engine.h
+++ Engine.h
@@ -28,6 +28,7 @@ const int timer_id = WM_USER + 1;
 
 //------------------------------------------------------------------------------------------------------------
 class C_engine;
+class C_level;
 
 //------------------------------------------------------------------------------------------------------------
 class C_ball
@@ -35,8 +36,8 @@ class C_ball
 public:
     C_ball();
 
-    void F_draw(HDC hdc, RECT& paint_area, C_engine* engine);
-    void F_move(C_engine* engine);
+    void F_draw(HDC hdc, RECT &paint_area, C_engine *engine);
+    void F_move(C_engine *engine, C_level *level);
 
     static const int   ball_size = 4;
 
@@ -49,7 +50,33 @@ private:
 
     RECT        ball_rect;
     RECT        prev_ball_rect;
-};
+};// end class
+
+//------------------------------------------------------------------------------------------------------------
+class C_level
+{
+public:
+
+    void F_check_level_brick_hit(int& next_y_pos, double& ball_direction);
+    void F_draw_brick(HDC hdc, int x, int y, E_brick_type brick_type);
+    void F_set_brick_letter_color(bool is_switch_color, HPEN& front_pen, HBRUSH& front_brush, HPEN& back_pen, HBRUSH& back_brush);
+    void F_draw_brick_letter(HDC hdc, int x, int y, E_brick_type brick_type, E_letter_type letter_type, int rotation_step);
+    void F_draw(HDC hdc, RECT& paint_area);
+
+    static const int   level_width = 12;
+    static const int   level_height = 14;
+    static const int   level_x_offset = 8;
+    static const int   level_y_offset = 6;
+    static const int   cell_width = 16;
+    static const int   cell_height = 8;
+    static const int   brick_width = 15;
+    static const int   brick_height = 7;
+
+    HPEN pen_white_fat, pen_light_red, pen_cyan;
+    HBRUSH brush_light_red, brush_cyan;
+
+    RECT level_rect;
+};// end class
 
 //------------------------------------------------------------------------------------------------------------
 class C_engine
@@ -61,15 +88,8 @@ public:
     void F_draw_frame   (HDC hdc, RECT &paint_area);
     int F_on_key_down   (E_key_type key_type);
     int F_on_timer      ();
-    void F_check_level_brick_hit(int& next_y_pos);
 
-    static const int   level_width = 12;
-    static const int   level_height = 14;
-    static const int   level_x_offset = 8;
-    static const int   level_y_offset = 6;
-    static const int   cell_width = 16;
-    static const int   cell_height = 8;
-    static const int   max_x_pos = level_x_offset + cell_width * level_width;
+    static const int   max_x_pos = C_level::level_x_offset + C_level::cell_width * C_level::level_width;
     static const int   max_y_pos = 199 - C_ball::ball_size;
     static const int   y_pos_platform = 185;
     static const int   border_x_offset = 6;
@@ -87,27 +107,21 @@ public:
 private:
     void F_create_pen_brush         (unsigned char r, unsigned char g, unsigned char b, HPEN &pen, HBRUSH &brush);
     void F_redraw_platform          ();
-    void F_draw_brick               (HDC hdc, int x, int y, E_brick_type brick_type);
-    void F_set_brick_letter_color   (bool is_switch_color, HPEN &front_pen, HBRUSH &front_brush, HPEN &back_pen, HBRUSH &back_brush);
-    void F_draw_brick_letter        (HDC hdc, int x, int y, E_brick_type brick_type, E_letter_type letter_type, int rotation_step);
-    void F_draw_level               (HDC hdc);
     void F_draw_platform            (HDC hdc, int x, int y);
     void F_draw_border              (HDC hdc, int x, int y, bool top_border);
     void F_draw_bounds              (HDC hdc, RECT &paint_area);
 
-    static const int   brick_width = 15;
-    static const int   brick_height = 7;
     static const int   circle_size = 7;
     static const int   platform_height = 7;
 
     int         inner_width;
 
-    HPEN        pen_white_fat, pen_black, pen_light_red, pen_cyan, pen_dark_red, pen_blue;
-    HBRUSH      brush_black, brush_light_red, brush_cyan, brush_dark_red, brush_blue;
+    HPEN        pen_white_fat, pen_light_red, pen_cyan, pen_black, pen_dark_red, pen_blue;
+    HBRUSH      brush_light_red, brush_cyan, brush_black, brush_dark_red, brush_blue;
 
     RECT        platform_rect, prev_platform_rect;
-    RECT        level_rect;
 
     C_ball ball;
-}; // end class
+    C_level level;
+};// end class
 
